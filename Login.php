fe12f21e4089b7607f638f180d91caa4d054731d
Tamara <48566400+tamaraAH4@users.noreply.github.com>
2021-05-07 18:25:20 +0200

    Add files via upload

---
+++ Login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="es"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <style type="text/css">
+ .sectionprincipal{
+    
+    margin-left: 20%;
+    margin-right: 20%;
+    margin-top: 5%;
+}
+#section1{
+    display: flex;
+    justify-content: center;
+    background-color: rgb(236, 236, 134);
+}
+@media all and (max-width: 300px) {
+    .sectionprincipal{
+        margin-left: 0%;
+        margin-right: 0%;
+        margin-top: 0%;
+    }
+}
+  </style>
+</head>
+
+<body>
+<section class="sectionprincipal">
+  <h2>¿Eres ya usuario?</h2>
+    <section id="section1">
+    <form action="index.php" method="post"> <!-- Indico que los datos se procesarán en index.php -->
+    <hr>
+      <h2>Login</h2>
+        <input type="text" name="user" placeholder="Usuario" require=""><br><br> <!-- Pido usuario y contraseña -->
+        <input type="password" name="password" placeholder="Contraseña" require=""><br><br>
+        <input type="submit" value="Entrar" name="registrar"> <!-- Creo botón de entrar si ya está registrado -->
+        <hr>
+    </form>
+  </section>
+  <section id="section1">
+    <form action="register.php" method="post"> <!-- Indico que los datos se procesarán en register.php si no está registrado para que le llevé a la página donde se rellenan dichos datos-->
+      <p>¿Aún no eres usuario? <input type="submit" value="Crear cuenta"></p>
+    </form>
+  </section>
+  </section>
+
+
+</body></html>
---
+++ register.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register.php</title>
+    <style>
+        .sectionprincipal {
+            margin-left: 20%;
+            margin-right: 20%;
+            margin-top: 5%;
+        }
+
+        #section1 {
+            background-color: rgb(236, 236, 134);
+            display: flex;
+            justify-content: center;
+        }
+
+        legend {
+            float: left;
+            font-weight: bold;
+        }
+
+        @media all and (max-width: 300px) {
+            .sectionprincipal {
+                margin-left: 0%;
+                margin-right: 0%;
+                margin-top: 0%;
+            }
+
+        }
+    </style>
+</head>
+
+<body>
+    <!--Creamos un formulario que envía los datos a index.php, este formulario contiene los campos que después almacenaremos en index.php en las cookies.-->
+    <section class="sectionprincipal">
+        <h1>Formulario de registro</h1>
+        <section id="section1">
+            <form class="form" action="index.php" method="POST">
+                <hr>
+                <legend>Regístrate</legend><br><br>
+                <input type="text" name="user" placeholder="Usuario" require></input><br><br>
+                <input type="password" name="password" placeholder="Contraseña" require></input><br><br>
+                <input type="text" name="biografia" placeholder="Biografía" require><br><br>
+                <input type="date" name="birthDate"><br><br>
+                <input type="text" name="nacionalidad" placeholder="Nacionalidad" require><br><br>
+                <select name="sexo">
+                    <option value="Hombre">Hombre</option>
+                    <option value="Mujer">Mujer</option>
+                    <option value="No Binario">No Binario</option>
+                </select>
+                <input type="checkbox" require>Aceptar términos y condiciones<br><br>
+                <hr>
+                <input type="submit" value="Registrar" name="registrar">
+            </form>
+        </section>
+    </section>
+</body>
+
+</html>
