5ccf37dfdb10cc031193af9bf3c80dbc271f60d2
TheCrasterX <48164358+TheCrasterX@users.noreply.github.com>
2021-05-13 22:27:53 +0200

    Add files via upload

--- onlyfilms.sql
+++ onlyfilms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-05-2021 a las 23:45:07
+-- Tiempo de generación: 13-05-2021 a las 22:26:48
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.11
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `onlyfilms`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `only_films`
+--
+
+CREATE TABLE `only_films` (
+  `id` int(5) NOT NULL,
+  `Titulo` varchar(80) NOT NULL,
+  `Foto` varchar(500) DEFAULT NULL,
+  `Sinopsis` varchar(500) DEFAULT NULL,
+  `Genero` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `only_films`
+--
+
+INSERT INTO `only_films` (`id`, `Titulo`, `Foto`, `Sinopsis`, `Genero`) VALUES
+(1, 'Salvar al soldado Ryan', 'https://es.web.img3.acsta.net/pictures/14/03/05/09/42/163621.jpg', 'Tras el desembarco de Normandía durante la Segunda Guerra Mundial, donde murieron muchos combatientes aliados, un grupo de soldados americanos es elegido para llevar a cabo su última misión: salvar a un solo hombre que se encuentra al otro lado de las líneas enemigas.', 'Accion'),
+(2, 'El resplandor', 'https://static2.elcorreo.com/www/multimedia/202005/25/media/cortadas/el-resplandor-kKKH-U1102973139567IB-624x385@RC.jpg', 'La película relata la historia de Jack Torrance, un exprofesor que acepta un puesto como vigilante de invierno en un solitario hotel de alta montaña para ocuparse del mantenimiento. Al poco tiempo de haberse instalado allí junto con su esposa y su hijo, empieza a sufrir inquietantes trastornos de personalidad.', 'Terror'),
+(3, 'The Fast and the Furious', 'https://images-na.ssl-images-amazon.com/images/I/71qtDoM-rcL._SY679_.jpg', 'La vida de Dominic Toretto (Vin Diesel) está marcada día y noche por la velocidad. Durante el día prepara y tunea coches de carreras de alta gama, mejora sus prestaciones y los embellece. ... Rápidamente se ganará la confianza de Toretto, pero todo se complicará cuando aparezca su hermana y se enamora de ella.', 'Accion'),
+(4, 'Alien: el octavo pasajero', 'https://pics.filmaffinity.com/Alien_el_octavo_pasajero-172055367-large.jpg', 'La nave Nostromo se dirige a la Tierra tras una misión convencional de extracción minera con sus tripulantes en estado de hibernación. Sin embargo, el ordenador principal decide despertarlos antes de tiempo ante la recepción de una señal desconocida.', 'Ciencia Ficción'),
+(5, 'El diario de Noa', 'https://i.blogs.es/4c2684/the-notebook-netflix-cambia-el-final/840_560.jpg', 'El libro cuenta la vida de Noah Calhoun y Allie Nelson, dos jóvenes adolescentes de Carolina del Norte que, a pesar de vivir en dos ambientes sociales muy diferentes, pasan un verano idílico juntos y profundamente enamorados, antes de ser separados, primero por sus padres, y más tarde por la Segunda Guerra Mundial.', 'Romantica'),
+(6, 'Regreso al futuro', 'https://pics.filmaffinity.com/Regreso_al_futuro-139415628-large.jpg', 'La cinta transcurre en el año 1985, una época en la que el joven Marty McFly lleva una existencia anónima con su novia Jennifer. Los únicos problemas son su familia en crisis y un director al que le encantaría expulsarle del instituto, por lo que deberá hacer todo lo que esté en su mano para revertir esa situación y aparentar total normalidad. Amigo del excéntrico profesor Emmett Brown, una noche le acompaña a probar su nuevo experimento: viajar en el tiempo usando un DeLorean modificado. Sin du', 'Ciencia Ficcion'),
+(7, 'Psicosis', 'https://lh3.googleusercontent.com/proxy/0QLf0WbhAPsg1l9cGQCbbDyRhpI372GCHaxscfpuL3Fnqq72UjTW8yEQVpMkH7c3VLj7XpX9fRpfNYP8vDJoQXlLHu8QXNCq7ji0h3xbIkP3o_xOr2b6CgVi1HJFy0bj', 'Inspirada en la novela homónima de Robert Bloch, la historia está ambientada en un tétrico motel de carretera llamado cuyo dueño es Norman Bates (Anthony Perkins, \'Asesinato en el Orient Express\'). Junto al motel hay una casa, tan poco acogedora como el edificio, en la que reside el señor Bates con su madre.', 'Terror');
 
 -- --------------------------------------------------------
 
@@ -83,6 +110,12 @@ INSERT INTO `only_users` (`id`, `usuario`, `pass`, `email`, `fechNacimiento`) VA
 --
 
 --
+-- Indices de la tabla `only_films`
+--
+ALTER TABLE `only_films`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `only_post`
 --
 ALTER TABLE `only_post`
@@ -99,6 +132,12 @@ ALTER TABLE `only_users`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `only_films`
+--
+ALTER TABLE `only_films`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `only_post`
--- peliculas.php
+++ peliculas.php
@@ -10,19 +10,84 @@
     <title>Peliculas</title>
 </head>
 <body>
+    <!--AQUI GENERO LA LISTA DE PELICULAS DE ACCION -->
     <?php
     $arrayTitulo = array();
-    $sql='SELECT titulo FROM only_films';
+    $sql='SELECT titulo FROM only_films WHERE genero="Accion"';
     $MyBBDD->consulta($sql);
     while ($fila = $MyBBDD->extraer_registro()) {
         foreach ($fila as $indice => $valor) {
             array_push($arrayTitulo, $valor);
         }
     }
+    echo'<ul>';
     for ($i=0; $i < count($arrayTitulo); $i++) { 
-        echo $arrayTitulo[$i];
-        echo '<br>';
+        echo '<li>'.$arrayTitulo[$i].'</li>';
     }
+    echo'</ul>';
+    unset($arrayTitulo);
+    ?>
+    <!--AQUI GENERO LA LISTA DE PELICULAS DE TERROR -->
+    <?php
+    $arrayTitulo = array();
+    $sql='SELECT titulo FROM only_films WHERE genero="Terror"';
+    $MyBBDD->consulta($sql);
+    while ($fila = $MyBBDD->extraer_registro()) {
+        foreach ($fila as $indice => $valor) {
+            array_push($arrayTitulo, $valor);
+        }
+    }
+    echo'<ul>';
+    for ($i=0; $i < count($arrayTitulo); $i++) { 
+        echo '<li>'.$arrayTitulo[$i].'</li>';
+    }
+    echo'</ul>';
+    /*$arrayImagenes = array();
+    $sql='SELECT Foto FROM only_films';
+    $MyBBDD->consulta($sql);
+    while ($fila = $MyBBDD->extraer_registro()) {
+        foreach ($fila as $indice => $valor) {
+            array_push($arrayImagenes, $valor);
+        }
+    }*/
+    /*for ($i=0; $i < count($arrayImagenes); $i++) { 
+        echo '<img src="'.$arrayImagenes[$i].'">';
+    }*/
+    ?>
+    <!--AQUI GENERO LA LISTA DE PELICULAS DE CIENCIA FICCION -->
+    <?php
+    $arrayTitulo = array();
+    $sql='SELECT titulo FROM only_films WHERE genero="Ciencia Ficcion"';
+    $MyBBDD->consulta($sql);
+    while ($fila = $MyBBDD->extraer_registro()) {
+        foreach ($fila as $indice => $valor) {
+            array_push($arrayTitulo, $valor);
+        }
+    }
+    echo'<ul>';
+    for ($i=0; $i < count($arrayTitulo); $i++) { 
+        echo '<li>'.$arrayTitulo[$i].'</li>';
+    }
+    echo'</ul>';
+    unset($arrayTitulo);
+    ?>
+
+    <!--AQUI GENERO LA LISTA DE PELICULAS ROMANTICAS -->
+    <?php
+    $arrayTitulo = array();
+    $sql='SELECT titulo FROM only_films WHERE genero="Romantica"';
+    $MyBBDD->consulta($sql);
+    while ($fila = $MyBBDD->extraer_registro()) {
+        foreach ($fila as $indice => $valor) {
+            array_push($arrayTitulo, $valor);
+        }
+    }
+    echo'<ul>';
+    for ($i=0; $i < count($arrayTitulo); $i++) { 
+        echo '<li>'.$arrayTitulo[$i].'</li>';
+    }
+    echo'</ul>';
+    unset($arrayTitulo);
     ?>
 </body>
 </html>
