0f7df18fb5e945fbb4dd38eb5c55390923ac2b1e
Tamara <48566400+tamaraAH4@users.noreply.github.com>
2021-05-17 17:15:52 +0200

    Add files via upload

--- onlyfilms.sql
+++ onlyfilms.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-05-2021 a las 22:26:48
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.11
+-- Tiempo de generación: 17-05-2021 a las 17:13:24
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.3.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -46,7 +47,8 @@ INSERT INTO `only_films` (`id`, `Titulo`, `Foto`, `Sinopsis`, `Genero`) VALUES
 (4, 'Alien: el octavo pasajero', 'https://pics.filmaffinity.com/Alien_el_octavo_pasajero-172055367-large.jpg', 'La nave Nostromo se dirige a la Tierra tras una misión convencional de extracción minera con sus tripulantes en estado de hibernación. Sin embargo, el ordenador principal decide despertarlos antes de tiempo ante la recepción de una señal desconocida.', 'Ciencia Ficción'),
 (5, 'El diario de Noa', 'https://i.blogs.es/4c2684/the-notebook-netflix-cambia-el-final/840_560.jpg', 'El libro cuenta la vida de Noah Calhoun y Allie Nelson, dos jóvenes adolescentes de Carolina del Norte que, a pesar de vivir en dos ambientes sociales muy diferentes, pasan un verano idílico juntos y profundamente enamorados, antes de ser separados, primero por sus padres, y más tarde por la Segunda Guerra Mundial.', 'Romantica'),
 (6, 'Regreso al futuro', 'https://pics.filmaffinity.com/Regreso_al_futuro-139415628-large.jpg', 'La cinta transcurre en el año 1985, una época en la que el joven Marty McFly lleva una existencia anónima con su novia Jennifer. Los únicos problemas son su familia en crisis y un director al que le encantaría expulsarle del instituto, por lo que deberá hacer todo lo que esté en su mano para revertir esa situación y aparentar total normalidad. Amigo del excéntrico profesor Emmett Brown, una noche le acompaña a probar su nuevo experimento: viajar en el tiempo usando un DeLorean modificado. Sin du', 'Ciencia Ficcion'),
-(7, 'Psicosis', 'https://lh3.googleusercontent.com/proxy/0QLf0WbhAPsg1l9cGQCbbDyRhpI372GCHaxscfpuL3Fnqq72UjTW8yEQVpMkH7c3VLj7XpX9fRpfNYP8vDJoQXlLHu8QXNCq7ji0h3xbIkP3o_xOr2b6CgVi1HJFy0bj', 'Inspirada en la novela homónima de Robert Bloch, la historia está ambientada en un tétrico motel de carretera llamado cuyo dueño es Norman Bates (Anthony Perkins, \'Asesinato en el Orient Express\'). Junto al motel hay una casa, tan poco acogedora como el edificio, en la que reside el señor Bates con su madre.', 'Terror');
+(7, 'Psicosis', 'https://lh3.googleusercontent.com/proxy/0QLf0WbhAPsg1l9cGQCbbDyRhpI372GCHaxscfpuL3Fnqq72UjTW8yEQVpMkH7c3VLj7XpX9fRpfNYP8vDJoQXlLHu8QXNCq7ji0h3xbIkP3o_xOr2b6CgVi1HJFy0bj', 'Inspirada en la novela homónima de Robert Bloch, la historia está ambientada en un tétrico motel de carretera llamado cuyo dueño es Norman Bates (Anthony Perkins, \'Asesinato en el Orient Express\'). Junto al motel hay una casa, tan poco acogedora como el edificio, en la que reside el señor Bates con su madre.', 'Terror'),
+(8, 'Leyendas de Pasión', 'https://r4.abcimg.es/resizer/resizer.php?imagen=https%3A%2F%2Fstatic1.abc.es%2Fmedia%2Fpeliculas%2F000%2F000%2F006%2Fleyendas-de-pasion-1.jpg&nuevoancho=690&medio=abc', 'William Ludlow (Anthony Hopkins), un coronel abandonado por su mujer, ha criado a sus tres hijos en su gran rancho de Montana, a los pies de las Montañas Rocosas. La Primera Guerra Mundial (1914-1918) y una bella mujer cambiarán el destino de todos ellos para siempre. Sus hijos son el indómito Tristan (Brad Pitt), el sensato Alfred (Aidan Quinn) y Samuel (Henry Thomas), el menor, que acaba de traer al rancho a su bella prometida Susannah (Julia Ormond).', 'Romantica');
 
 -- --------------------------------------------------------
 
@@ -137,7 +139,7 @@ ALTER TABLE `only_users`
 -- AUTO_INCREMENT de la tabla `only_films`
 --
 ALTER TABLE `only_films`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `only_post`
