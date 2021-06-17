75defac425ac63731f30e34d087d5a8d995ab569
Tamara <48566400+tamaraAH4@users.noreply.github.com>
2021-06-17 20:04:33 +0200

    Add files via upload

--- perfil.php
+++ perfil.php
@@ -16,10 +16,10 @@
     <div class="navegador">
         <nav class="navUno">
         <img class="logo" src="logo.png">
-            <p><a href="feed.php">Inicio</a></p>
+            <p><a href="index.php">Inicio</a></p>
             <p><a href="perfil.php">Perfil</a></p>
             <p><a href="peliculas.php">Peliculas</a></p>
-            <p><a href="index.php">Cerrar Sesión</a></p>
+            <p><a href="login.php">Cerrar Sesión</a></p>
         </nav>
     </div>
     <br><br><br><br><br> 
@@ -82,7 +82,7 @@
 
                     for ($i=0; $i <= 9 ; $i++) { 
                         echo '<div class="divComentarios">';
-                        echo '<p class="post"> '.$arrayPost[$i].'</p>';
+                        echo '<p class="post2"> '.$arrayPost[$i].'</p>';
                         echo '</div>';    
                     }
                 ?>
